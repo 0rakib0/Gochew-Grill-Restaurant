0ff4a2625afad54b3c60b0ff4fa438e374194378
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-28 03:20:24 +0600

    create section title and style it

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const SectionTitle = ({heading, SubHeading}) =>{
+    return (
+        <div className="mt-20 mb-8 text-center md:w-4/12 mx-auto">
+            <p className="text-yellow-500">--- {SubHeading} ---</p>
+            <h4 className="border-y border-yellow-500 my-4 py-2 text-4xl font-semibold">{heading}</h4>
+        </div>
+    )
+}
+
+export default SectionTitle
--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -3,15 +3,20 @@ import { FreeMode, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
- import image1 from '../../assets/home/slide1.jpg'
- import image2 from '../../assets/home/slide2.jpg'
- import image3 from '../../assets/home/slide3.jpg'
- import image4 from '../../assets/home/slide4.jpg'
- import image5 from '../../assets/home/slide5.jpg'
+import image1 from '../../assets/home/slide1.jpg'
+import image2 from '../../assets/home/slide2.jpg'
+import image3 from '../../assets/home/slide3.jpg'
+import image4 from '../../assets/home/slide4.jpg'
+import image5 from '../../assets/home/slide5.jpg'
+import SectionTitle from '../../Components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
-        <>
+        <section>
+            <SectionTitle
+            SubHeading={'From 11:00AM To 10:00PM'}
+            heading={'ORDER ONLINE'}
+            ></SectionTitle>
             <Swiper
                 slidesPerView={3.5}
                 spaceBetween={10}
@@ -20,7 +25,7 @@ const Category = () => {
                     clickable: true,
                 }}
                 modules={[FreeMode, Pagination]}
-                className="mySwiper"
+                className="mySwiper mb-24"
             >
                 <SwiperSlide>
                     <img src={image1} alt="" />
@@ -42,9 +47,9 @@ const Category = () => {
                     <img src={image5} alt="" />
                     <p className='text-3xl -mt-12 text-white text-center drop-shadow-lg'>CACE</p>
                 </SwiperSlide>
-                
+
             </Swiper>
-        </>
+        </section>
     )
 }
 

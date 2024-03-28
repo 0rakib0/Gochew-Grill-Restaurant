897798eec39afd71ab9cc45722ed5e9753b06250
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-29 05:32:43 +0600

    section title sub menus tranform to text italy

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='about flex items-center justify-center'>
             <div className='bg-white w-3/4 text-center p-8'>
                 <h5 className='text-4xl pb-4 uppercase'>Gochew Grill</h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque culpa rerum numquam praesentium error est, velit delectus at hic quo a, qui, enim perspiciatis debitis ullam ipsum unde magni. Aut dolor autem, eos hic delectus vel mollitia sequi dignissimos atque.</p>
+                <p>A restaurant is a business that prepares and serves food and drinks to customers.[1] Meals are generally served and eaten on the premises, but many restaurants also offer take-out and food delivery services. Restaurants vary greatly in appearance and offerings, including a wide variety of cuisines</p>
             </div>
         </div>
     )
---
+++ src/Pages/Home/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../Components/SectionTitle/SectionTitle"
+
+const PopularMenu = () =>{
+    return (
+        <div>
+            <SectionTitle
+            SubHeading={'Check it out'}
+            heading={'FROM OUR MENU'}
+            ></SectionTitle>
+        </div>
+    )
+}
+
+export default PopularMenu
